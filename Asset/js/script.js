f169fb43cab69273f45e26b0e9d6a944e5796877
Ph-uad <temidayo26j@gmail.com>
2022-06-05 14:35:33 +0100

    fitted

--- Asset/js/script.js
+++ Asset/js/script.js
@@ -1,46 +1,54 @@
-gsap.registerPlugin(ScrollTrigger)
-
 let tl = gsap.timeline();
 
-
-tl.from(".header-title", { duration: .2, opacity: 0, scale: 0.3, ease: "back", y: -200 });
+tl.from(".heading-jumbo__large", { duration: 1, opacity: 0, scale: 0.3, ease: "back", y: -200 });
 tl.from(".icon", { duration: .5, opacity: 0, y: -20, stagger: 0.5 });
-tl.from(" .slide-link__line", {opacity: 0});
+tl.from(" .slide-link__line", { opacity: 0 });
 
 
-gsap.to(".message-design__block", {
-    scrollTrigger: {
-        trigger: ".fluid-container",
-        start:"top, -50%",
-        scrub: 2,
+// gsap.to(".message-design__block", {
+//     scrollTrigger: {
+//         trigger: ".fluid-container",
+//         start:"top, -50%",
+//         scrub: 2,
 
-        toggleActions: "restart pause reverse pause"
-    },
-       scale: 0,
-        y: 2500,
-        x: 500
+//         toggleActions: "restart pause reverse pause"
+//     },
+//        scale: 0,
+//         y: 2500,
+//         x: 500
+// });
+
+
+const slides = document.querySelectorAll(".work-card");
+const nextSlide = document.querySelector(".btn-next");
+const prevSlide = document.querySelector(".btn-prev");
+let maxSlide = slides.length - 1;
+let curSlide = 0;
+
+
+slides.forEach((slide, index) => {slide.style.transform = `translateX(${index * 100}%)`;});
+
+nextSlide.addEventListener("click", function () {
+    if (curSlide === maxSlide) {
+        curSlide = 0;
+    } else {
+        curSlide++;
+    }
+
+    slides.forEach((slide, indx) => {
+        slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+    });
 });
 
+prevSlide.addEventListener("click", function () {
+    if (curSlide === 0) {
+        curSlide = maxSlide;
+    } else {
+        curSlide--;
+    }
 
-
-
-
-
-//navCircle
-// const navCircle = document.querySelector('.slide-link');
-// let counter = -1;
-
-// navCircle.addEventListener('click',function(e){
-
-//     //Create an array of link to allow dynamic use
-//     const linkArray = ['body','message', 'process', 'about', 'philosophy'];
-//     if(counter < linkArray.length - 1){
-//           counter ++;
-//     }else{
-//         counter = 0;
-//     }
-  
-//     e.target.href= `#${linkArray[counter]}`
-//     console.log( e.target.href, counter)
-
-// })
+    //   move slide by 100%
+    slides.forEach((slide, indx) => {
+        slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+    });
+});
