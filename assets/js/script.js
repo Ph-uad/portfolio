7374d09149287e594fc90ef49ca543e4123339b1
Ph-uad <temidayo26j@gmail.com>
2022-12-26 12:59:55 +0100

    adding sub context

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,23 @@
-const devProject = [
-    {img: './assets/image/numb-Skull.png',title: 'Numb Skull' , description: 'This was a web project that was aim at subtle animation and effects without much interactions by the user. Aimed to satisfy user experience'},
-    {img: './assets/image/uI-adaptation.png',title: 'UI Adaptation' , description: 'I was inspired by the design of _____ and desided to Develop and adapt their UIDesign as a challenge  '},
-    {img: './assets/image/Real-Estate.png',title: 'Real Estate' , description: 'I was inspired by the design of _____ and desided to Develop and adapt their UIDesign as a challenge  '},
+const container = document.querySelector('.option__container'),
+devProject = [
+    { img: './assets/image/dev/numb-Skull.png', title: 'Numb Skull', description: 'This was a web project that was aim at subtle animation and effects without much interactions by the user. Aimed to satisfy user experience' },
+    { img: './assets/image/dev/uI-adaptation.png', title: 'UI Adaptation', description: 'I was inspired by the design of _____ and desided to Develop and adapt their UIDesign as a challenge  ' },
+    { img: './assets/image/dev/Real-Estate.png', title: 'Real Estate', description: 'I was inspired by the design of _____ and desided to Develop and adapt their UIDesign as a challenge  ' },
+],
+designProject = [
+    { img: './assets/image/design/gentilhomme.jpg', title: 'Gentilhomme', description: 'This was an initial try at web design project that was aim at better respresntation a product, with the concept of maintaining simplicity, effective design, ensure ease of use and still look decent enough to be in trend' },
+    { img: './assets/image/design/UInumbskull.jpg', title: 'UI NumbSkull', description: 'The process of design first before developing ' },
+    { img: './assets/image/design/WireFrame.jpg', title: 'WireFraming', description: 'I was inspired by the design of _____ and desided to Develop and adapt their UIDesign as a challenge  ' },
 ]
+writingProject = [
+    { img: './assets/image/articles/DigitalTransformation.jpg', title: 'Digital Transformation', description: 'This was an initial try at web design project that was aim at better respresntation a product, with the concept of maintaining simplicity, effective design, ensure ease of use and still look decent enough to be in trend' },
+    { img: './assets/image/articles/RolestoDigitalTransformation.jpg', title: 'Roles to Digital Transformation', description: 'The process of design first before developing ' },
+    { img: './assets/image/articles/SteeringCommittee.jpg', title: 'Steering Committee', description: 'I was inspired by the design of _____ and desided to Develop and adapt their UIDesign as a challenge  ' },
+];
 
-const designProject = [
-    {img: './assets/image/gentilhomme.jpg',title: 'Gentilhomme' , description: 'This was an initial try at web design project that was aim at better respresntation a product, with the concept of maintaining simplicity, effective design, ensure ease of use and still look decent enough to be in trend'},
-    {img: './assets/image/UInumbskull.jpg',title: 'UI NumbSkull' , description: 'The process of design first before developing '},
-    {img: './assets/image/WireFrame.jpg',title: 'WireFraming' , description: 'I was inspired by the design of _____ and desided to Develop and adapt their UIDesign as a challenge  '},
-]
-
-const widget = document.querySelector('#widget')
-const UIwidget = document.querySelector('#UI-widget')
- 
-const jobTile = (project) =>{
-    return(
-     `<div class="tile center">
+const jobTile = (project) => {
+    return (
+        `<div class="tile center">
             <div class="container flex">
                 <figure class="figure">
                     <img src='${project.img}' class='img' alt="project thumbnail" />
@@ -30,14 +32,31 @@ const jobTile = (project) =>{
 }
 
 
-const appendArrayToDom = (object, destination) =>{
+const appendArrayToDom = (object, destination) => {
     object.forEach(item => {
-        destination.insertAdjacentHTML("afterbegin",jobTile(item))
+        destination.insertAdjacentHTML("afterbegin", jobTile(item))
     })
 }
 
-console.log(widget, UIwidget)
+const fixDomContent = (idName = '', object = devProject) => {
+    container.setAttribute('id', idName);
+    container.innerHTML = ''
+    appendArrayToDom(object, container)
+}
 
-// appendArrayToDom(devProject, widget)
-appendArrayToDom(designProject, UIwidget)
+document.querySelector('#Dev--Link').addEventListener('click', (e) => {
+    e.preventDefault();
+    fixDomContent('widget', devProject)
+})
 
+document.querySelector('#UI--Link').addEventListener('click', (e) => {
+    e.preventDefault();
+    fixDomContent('UI-widget', designProject)
+})
+
+document.querySelector('#Writing--Link').addEventListener('click', (e) => {
+    e.preventDefault();
+    fixDomContent('Writing-widget', writingProject)
+})
+
+fixDomContent()
